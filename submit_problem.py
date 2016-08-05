6d52ffbeb93e2f72da8edd5580446c7dcb13fd1e
elliotc12 <elliotc12@gmail.com>
2016-08-05 16:38:59 -0700

    Make submit_problem.py read in submission file in format Submissions/id-text.txt

--- submit_problem.py
+++ submit_problem.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python2.7
+
 import subprocess
 import sys
 
@@ -6,7 +7,22 @@ API_KEY = "279-4ada75226794d3f7e114f6408ad14c73"
 HW_ENDPOINT = "http://2016sv.icfpcontest.org/api/hello"
 SUBMIT_ENDPOINT = "http://2016sv.icfpcontest.org/api/solution/submit"
 
-def main(problem_id, solution_file):
+def main():
+    if (len(sys.argv) != 2):
+        print "Usage: ./submit_problem.py problem_file"
+        exit(1)
+    if (str.find(sys.argv[1], '-') == -1):
+        print "Solution file format: id-infostring.txt"
+        exit(1)
+
+    solution_file = sys.argv[1]
+    if (str.find(solution_file, '/') != -1):
+        id_start_idx = len(solution_file) - 1 - str.index(solution_file[::-1], '/')
+    else:
+        id_start_idx = 0
+    id_end_idx = str.index(solution_file, '-')
+    problem_id = solution_file[id_start_idx+1:id_end_idx]
+
     cmd_list = [
         "curl",
         "--compressed",
@@ -17,13 +33,11 @@ def main(problem_id, solution_file):
         "-F", "solution_spec=@" + solution_file,
         SUBMIT_ENDPOINT
     ]
+
     print subprocess.list2cmdline(cmd_list)
     out_json = subprocess.check_output(cmd_list)
     print out_json
 
 if __name__ == "__main__":
-    if (len(sys.argv) != 3):
-        print "Usage: ./submit_problem PROBLEM_ID solution_file"
-        exit(1)
-    main(sys.argv[1], sys.argv[2])
+    main()
 
