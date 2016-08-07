7312b7dacafda36bb8871a0ab6aa2e2c58926d25
elliotc12 <elliotc12@gmail.com>
2016-08-07 04:25:00 -0700

    Fix up submit_problem.py script to store more info.

--- submit_problem.py
+++ submit_problem.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python2.7
 
+import datetime
 import hashlib
 import json
 import os
@@ -14,26 +15,32 @@ solution_hash_file = 'solution_hashes.txt'
 def main():
     solution_hash_text = open(solution_hash_file, 'r').read()
     if len(solution_hash_text) > 0:
-        solution_hash_list = eval(solution_hash_text)
+        solution_hash_dict = eval(solution_hash_text)
     else:
-        solution_hash_list = []
+        solution_hash_dict = {}
 
     solution_names = [f for f in os.listdir('solutions') if os.path.isfile('solutions/' + f)]
     solution_files = ['solutions/' + s for s in solution_names]
 
+    solution_hashes = [data[0] for data in solution_hash_dict.values()]
+
     i = 0
     for solution_file in solution_files:
         i += 1
-        print i, " / ", len(solution_files)
+        sys.stdout.write("(" + str(i) + "/" + str(len(solution_files)) + ") ")
         solution_text = open(solution_file, 'r').read()
-        if hash(solution_text) in solution_hash_list:
-            print "Not submitting, no change"
+        if str(hash(solution_text)) in solution_hashes:
+            sys.stdout.write("Not submitting, no change.\n")
             continue
         else:
-            print "Submitting", solution_file
-            submit(solution_file)
-            solution_hash_list.append(solution_text)
-            open(solution_hash_file, "w").write(str(solution_hash_list))
+            sys.stdout.write("Submitting " + str(solution_file) + ": ")
+            (solution_id, result_bool, result_msg) = submit(solution_file)
+            result = str(result_bool)
+
+            data_str_list = [str(hash(solution_text)), result, result_msg, str(datetime.datetime.now())]
+            solution_hash_dict[solution_id] = data_str_list
+            open(solution_hash_file, "w").write(str(solution_hash_dict))
+            sys.stdout.write(result_msg + '\n')
         time.sleep(3.6)
 
 def submit(solution_file):
@@ -57,8 +64,11 @@ def submit(solution_file):
     ]
 
     submit_return_json = subprocess.check_output(cmd_list)
-    submit_return_list = json.loads(submit_return_json)
-    print submit_return_list
+    submit_return_dict = json.loads(submit_return_json)
+    if (submit_return_dict[u'ok'] == 0):
+        return (problem_id, False, submit_return_dict[u'error'])
+    else:
+        return (problem_id, True, submit_return_dict[u'resemblance'])
 
 if __name__ == "__main__":
     main()
