fa1e0e354021d87110a420211c20813a103729dd
elliotc12 <elliotc12@gmail.com>
2016-08-06 18:25:38 -0700

    Fix solution submitter.

--- submit_problem.py
+++ submit_problem.py
@@ -4,40 +4,47 @@ import hashlib
 import os
 import subprocess
 import sys
+import time
 
 import teaminfo
 
-submission_hash_file = 'submission_hashes.txt'
+solution_hash_file = 'solution_hashes.txt'
 
 def main():
-    submission_hash_text = open(submission_hash_file, 'r').read()
-    if len(submission_hash_text) > 0:
-        submission_hash_list = eval(submission_hash_text)
+    solution_hash_text = open(solution_hash_file, 'r').read()
+    if len(solution_hash_text) > 0:
+        solution_hash_list = eval(solution_hash_text)
     else:
-        submission_hash_list = []
+        solution_hash_list = []
 
-    submission_names = [f for f in os.listdir('submissions') if os.path.isfile('submissions/' + f)]
-    submission_files = ['submissions/' + s for s in submission_names]
+    solution_names = [f for f in os.listdir('solutions') if os.path.isfile('solutions/' + f)]
+    solution_files = ['solutions/' + s for s in solution_names]
 
-    for submission_file in submission_files:
-        submission_text = open(submission_file, 'r').read()
-        if hash(submission_text) in submission_hash_list:
+    i = 0
+    for solution_file in solution_files:
+        i += 1
+        print i, " / ", len(solution_files)
+        solution_text = open(solution_file, 'r').read()
+        if hash(solution_text) in solution_hash_list:
+            print "Not submitting, no change"
             continue
         else:
-            print "Submitting", submission_file
-            submit(submission_file)
-            open(submission_hash_file, "w").write(str(submission_hash_list + [hash(submission_text)]))
+            print "Submitting", solution_file
+            submit(solution_file)
+            open(solution_hash_file, "w").write(str(solution_hash_list + [hash(solution_text)]))
+        time.sleep(3.6)
 
 def submit(solution_file):
     if (str.find(solution_file, '/') != -1):
         id_start_idx = len(solution_file) - 1 - str.index(solution_file[::-1], '/')
     else:
         id_start_idx = 0
-    id_end_idx = str.index(solution_file, '-')
+    id_end_idx = str.index(solution_file, '.')
     problem_id = solution_file[id_start_idx+1:id_end_idx]
 
     cmd_list = [
         "curl",
+        "-s",
         "--compressed",
         "-L",
         "-H", "Expect:",
@@ -47,7 +54,6 @@ def submit(solution_file):
         teaminfo.PROBLEM_SUBMIT_ENDPOINT
     ]
 
-    print subprocess.list2cmdline(cmd_list)
     out_json = subprocess.check_output(cmd_list)
     print out_json
 
