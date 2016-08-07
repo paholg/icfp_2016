a4b63d506fbcdad39d0b2c5f42d7ead0a7f23acb
elliotc12 <elliotc12@gmail.com>
2016-08-06 18:04:24 -0700

    Make submit_problem.py submit everything new in submissions/

--- submit_problem.py
+++ submit_problem.py
@@ -1,19 +1,34 @@
 #!/usr/bin/python2.7
 
+import hashlib
+import os
 import subprocess
 import sys
 
 import teaminfo
 
-def main():
-    if (len(sys.argv) != 2):
-        print "Usage: ./submit_problem.py problem_file"
-        exit(1)
-    if (str.find(sys.argv[1], '-') == -1):
-        print "Solution file format: id-infostring.txt"
-        exit(1)
+submission_hash_file = 'submission_hashes.txt'
 
-    solution_file = sys.argv[1]
+def main():
+    submission_hash_text = open(submission_hash_file, 'r').read()
+    if len(submission_hash_text) > 0:
+        submission_hash_list = eval(submission_hash_text)
+    else:
+        submission_hash_list = []
+
+    submission_names = [f for f in os.listdir('submissions') if os.path.isfile('submissions/' + f)]
+    submission_files = ['submissions/' + s for s in submission_names]
+
+    for submission_file in submission_files:
+        submission_text = open(submission_file, 'r').read()
+        if hash(submission_text) in submission_hash_list:
+            continue
+        else:
+            print "Submitting", submission_file
+            submit(submission_file)
+            open(submission_hash_file, "w").write(str(submission_hash_list + [hash(submission_text)]))
+
+def submit(solution_file):
     if (str.find(solution_file, '/') != -1):
         id_start_idx = len(solution_file) - 1 - str.index(solution_file[::-1], '/')
     else:
